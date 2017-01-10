8b3bfae777b04925d2b72c85d993385c2e757b3f
Adrian Mowat <adrian.mowat@gmail.com>
2017-01-10 21:38:07 +0000

    Added starting points to read model

--- lib/hills/read_model.rb
+++ lib/hills/read_model.rb
@@ -11,13 +11,55 @@ module Hills
     end
 
     def load_event(event_name, payload)
+      id = payload[:id]
+
       case(event_name)
       when :new_munro_added
-        @hills << payload
+        @hills << Hill.new(payload)
       when :summit_latitude_added
-        self[payload[:id]]["summit_latitude"] = payload[:value]
+        self[id][:summit_latitude] = payload[:value]
       when :summit_longitude_added
-        self[payload[:id]]["summit_longitude"] = payload[:value]
+        self[id][:summit_longitude] = payload[:value]
+      when :starting_point_added
+        self[id].add_starting_point(payload[:name], payload[:latitude], payload[:longitude])
+      end
+    end
+  end
+
+  class Hill
+    attr_reader :starting_points
+
+    def initialize(id:, name:)
+      @hill = {id: id, name: name}
+      @starting_points = []
+    end
+
+    def add_starting_point(name, latitude, longitude)
+      @starting_points << Location.new(name, latitude, longitude)
+    end
+
+    def[](key)
+      @hill[key.to_sym]
+    end
+
+    def[]=(key, value)
+      @hill[key] = value
+    end
+  end
+
+  class Location
+    def initialize(name, latitude, longitude)
+      @name, @latitude, @longitude = name, latitude, longitude
+    end
+
+    def[](key)
+      case key
+      when "name"
+        @name
+      when "latitude"
+        @latitude
+      when "longitude"
+        @longitude
       end
     end
   end
--- test/test_hills_read_model.rb
+++ test/test_hills_read_model.rb
@@ -12,9 +12,12 @@ class HillsReadModelTest < Minitest::Test
     @read_model.load_event(:new_munro_added, id: "2", name: "Ben Chronzie")
 
     assert_equal(3, @read_model.hills.count)
-    assert_equal({id: "1", name: "Ben Vane"}, @read_model.hills[0])
-    assert_equal({id: "3", name: "Sgurr Fiona"}, @read_model.hills[1])
-    assert_equal({id: "2", name: "Ben Chronzie"}, @read_model.hills[2])
+    assert_equal("1", @read_model.hills[0]["id"])
+    assert_equal("Ben Vane", @read_model.hills[0]["name"])
+    assert_equal("3", @read_model.hills[1]["id"])
+    assert_equal("Sgurr Fiona", @read_model.hills[1]["name"])
+    assert_equal("2", @read_model.hills[2]["id"])
+    assert_equal("Ben Chronzie", @read_model.hills[2]["name"])
   end
 
   def test_adding_a_summit_latitude_and_longitude
@@ -27,5 +30,17 @@ class HillsReadModelTest < Minitest::Test
     assert_equal(4.56, @read_model["1"]["summit_longitude"])
     assert_nil(@read_model["3"]["summit_latitude"])
     assert_nil(@read_model["3"]["summit_longitude"])
+  end
+
+  def test_adding_a_starting_point
+    @read_model.load_event(:new_munro_added, id: "1", name: "Ben Vane")
+    @read_model.load_event(:new_munro_added, id: "3", name: "Sgurr Fiona")
+    @read_model.load_event(:starting_point_added, id: "3", name: "car park", latitude: 4.567, longitude: -1.432)
+
+    assert_equal 0, @read_model["1"].starting_points.count
+    assert_equal 1, @read_model["3"].starting_points.count
+    assert_equal "car park", @read_model["3"].starting_points.first["name"]
+    assert_equal 4.567, @read_model["3"].starting_points.first["latitude"]
+    assert_equal -1.432, @read_model["3"].starting_points.first["longitude"]
   end
 end
