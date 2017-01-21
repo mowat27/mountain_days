160a427d6cf0e189e6d15036ba8a9d24e40adcbf
Adrian Mowat <adrian.mowat@gmail.com>
2017-01-21 01:39:30 +0000

    Tided munro map using destructing

--- clients/munro-map/src/components/MunroMap.js
+++ clients/munro-map/src/components/MunroMap.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
 
 class MunroMap extends React.Component {
-  summitIcon(google, summit) {
+  summitIcon(summit) {
     let redPin = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FE7569'
     let greenPin = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|58D68D'
     return summit.selected ? greenPin : redPin
@@ -25,7 +25,7 @@ class MunroMap extends React.Component {
                 <Marker key={summit.hillnumber}
                   name={summit.name}
                   position={summit.position}
-                  icon={this.summitIcon(this.props.google, summit)}
+                  icon={this.summitIcon(summit)}
                 /> )
               )
             }
@@ -35,16 +35,15 @@ class MunroMap extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  let selected = state.selections.selectedHill
+const mapStateToProps = ({hills, selections}) => {
+  let selected = selections.selectedHill
   return {
-    summits: state.hills.map( (hill) => ({
+    summits: hills.map( (hill) => ({
       hillnumber: hill.hillnumber,
       name: hill.hillname,
       position: {lat: hill.summit.latitude, lng: hill.summit.longitude},
       selected: parseInt(hill.hillnumber) === selected
-    })),
-    ...state
+    }))
   }
 }
 
